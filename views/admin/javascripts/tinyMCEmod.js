c5a587339b9c86fa2753e0949a2e0c3d35f14492
Jeremy Guillette <jguillette@fas.harvard.edu>
2016-11-08 10:45:46 -0500

    changed from document ready to window load
    settings weren't being applied

--- views/admin/javascripts/tinyMCEmod.js
+++ views/admin/javascripts/tinyMCEmod.js
@@ -1,5 +1,5 @@
-jQuery(document).on('load',function() {
+jQuery(window).load(function() {
   tinyMCE.settings.verify_html = false;
   tinyMCE.settings.validate = false;
   tinyMCE.settings.plugins = "paste,inlinepopups,autoresize";
-})
+});
