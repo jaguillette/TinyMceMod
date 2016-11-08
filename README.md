821b541a31e0b047b315baf930fd77428ef1010d
Jeremy Guillette <jguillette@fas.harvard.edu>
2016-11-08 15:19:29 -0500

    replacing globals.js instead of trying to override
    Using a function to remove globals.js from the queue of js files, replacing it with tinyMCEmod.js, which is a modified copy.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 TinyMCE Mod
 ===========
 
-This plugin for Omeka is set up to overwrite some TinyMCE settings, and was made out of a desire to change the behavior of TinyMCE to make it more permissive of different HTML tags without changing the globals.js file in core Omeka. The plugin loads tinyMCEmod.js into the admin head, and sets three settings. Feel free to fork this and make your own changes to TinyMCE in the same way.
+This plugin for Omeka is set up to overwrite some TinyMCE settings, and was made out of a desire to change the behavior of TinyMCE to make it more permissive of different HTML tags without changing the globals.js file in core Omeka. This plugin removes globals.js from the queue of js files, and replaces it with tinyMCEmod.js. This file is a copy of globals.js, but with some modifications to the functionality of TinyMCE.
--- TinyMceModPlugin.php
+++ TinyMceModPlugin.php
@@ -1,13 +1,39 @@
 <?php
+
 class TinyMceModPlugin extends Omeka_Plugin_AbstractPlugin
 {
   protected $_hooks = array(
     'admin_head'
   );
 
+  /**
+   * Removes a previously appended script file.
+   * from http://stackoverflow.com/questions/18020622/how-to-remove-a-previously-added-script-file-in-a-zend-framework-controller
+   *
+   * @param string $src The source path of the script file.
+   * @return boolean Returns TRUE, if the removal has been a success.
+   */
+  public function removeScript($src) {
+      $headScriptContainer = Zend_View_Helper_Placeholder_Registry::getRegistry()
+              ->getContainer("Zend_View_Helper_HeadScript");
+      $iter = $headScriptContainer->getIterator();
+      $success = FALSE;
+      foreach ($iter as $k => $value) {
+          if(strpos($value->attributes["src"], $src) !== FALSE) {
+              $iter->offsetUnset($k);
+              $success = TRUE;
+          }
+      }
+      Zend_View_Helper_Placeholder_Registry::getRegistry()
+              ->setContainer("Zend_View_Helper_HeadScript",$headScriptContainer);
+      return $success;
+  }
+
   public function hookAdminHead($args)
   {
     queue_js_file('tinyMCEmod');
+    $old_globals_url = src('globals','javascripts','js');
+    $this->removeScript($old_globals_url);
   }
 }
 ?>
